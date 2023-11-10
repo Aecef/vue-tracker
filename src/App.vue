18601cf3ed163d87ac1669c101388f93c97cd87d
Alec Cleofe <aleccleofe@gmail.com>
2023-11-10 01:06:47 -0600

    Some Cleanup

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,18 @@ import { onMounted } from 'vue';
 import HelloWorld from '@components/HelloWorld.vue'
 import TheWelcome from '@components/TheWelcome.vue'
 import pb from '@components/PocketBase'
-import PocketBase from 'pocketbase'
 
-console.log(import.meta.env.VITE_ADMIN_EMAIL);
-console.log(import.meta.env.VITE_ADMIN_PASSWORD);
 
 onMounted (async () => {
+
   const authData = await pb.admins
     .authWithPassword(import.meta.env.VITE_ADMIN_EMAIL, import.meta.env.VITE_ADMIN_PASSWORD);
+
   const data = await pb.collection('users').getFullList().catch((err) => { console.log(err); });
 
-  console.log(pb.authStore.isValid);
-  console.log(data);
+  //console.log(pb.authStore.isValid);
+  //console.log(data);
+  
   pb.authStore.clear();
 });
 </script>
