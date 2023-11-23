6c01e954800675c9e03ff419d225d52848f5b6cb
Alec Cleofe <aleccleofe@gmail.com>
2023-11-23 15:42:37 -0600

    Update PocketBase.spec.js

--- src/components/__tests__/PocketBase.spec.js
+++ src/components/__tests__/PocketBase.spec.js
@@ -1,11 +1,22 @@
 import { describe, it, expect, test, toBe, instanceOf } from "vitest";
-import { mount } from "@vue/test-utils";
 import pb from "@components/PocketBase.js";
-import PocketBase from "pocketbase";
 import Client from "pocketbase";
 
 describe("PocketBase.js", () => {
   test("PocketBase instance is created", () => {
     expect(pb).instanceOf(Client);
+  });
+});
+describe("PocketBase Account Actions", () => {
+  test("Login and Logout", async () => {
+    const authData = await pb
+      .collection("users")
+      .authWithPassword(
+        import.meta.env.VITE_WEB_EMAIL,
+        import.meta.env.VITE_WEB_PASSWORD
+      );
+    expect(pb.authStore.isValid).toBe(true);
+    pb.authStore.clear();
+    expect(pb.authStore.isValid).toBe(false);
   });
 });
