15bd2485833d6d487cc3dd68dd70206ebc2d46df
Alec Cleofe <aleccleofe@gmail.com>
2023-12-22 15:59:21 -0600

    Update Home.vue
    - Adjusted Local Styling

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -43,28 +43,28 @@ export default{
 
 <template>
     <div class="home" id="home">
-        <div class="row p-lr">
+        <div class="row">
             <div class="col-md col-image pl-"></div>
-            <a href="/#/experience" class="col-md col-exp col-choice">
+            <a href="/#/experience" class="col-md" id="col-exp">
                 <p class="grid-font">EXPERIENCE</p>    
             </a>
             <div class="col-md col-image"></div>
         </div>
-        <div class="row p-lr">
-            <a href="/#/education" class="col-md col-iowa col-choice">
+        <div class="row">
+            <a href="/#/education" class="col-md" id="col-iowa">
                     <p class="grid-font">EDUCATION</p>    
             </a>
             <div class="col-md align-items-middle" id="col-title">
                 <h2 class="font-weight-bold grid-title text-light"> Alec Cleofe </h2>
                 <p class="text-light">Made by a <i>real</i> <b>go getter.</b> </p>
             </div>
-            <a href="/#/hobbies" class="col-md col-printer col-choice">
+            <a href="/#/hobbies" class="col-md" id="col-printer">
                 <p class="grid-font">LEARNING</p>    
             </a>
         </div>
-        <div class="row p-lr">
+        <div class="row">
             <div class="col-md col-image"></div>
-            <a href="/#/projects" class="col-md col-choice col-p">
+            <a href="/#/projects" class="col-md" id="col-p">
                 <p class="grid-font">PROJECTS</p>    
             </a>
             <div class="col-md col-image"></div>
@@ -103,67 +103,54 @@ export default{
     transition: 0.5s;
 }
 
+
+
+.col-image, #col-p, #col-exp, #col-printer, #col-iowa {
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+}
 .col-image{
-    background-image: url('@images/UoI.jpg');
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: center;
-    height: 100%;
+    /* background-image: url('@images/UoI.jpg'); */
 }
 
-.col-p{
+#col-p{
     background-image: url('@images/hitbox.jpg');
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: center;
-    height: 100%;
 }
-.col-exp{
+#col-exp{
     background-image: url('@images/exp.jpg');
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: center;
-    height: 100%;
 }
-.col-printer{
+#col-printer{
     background-image: url('@images/printer.jpg');
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: center;
-    height: 100%;
 }
-.col-iowa {
+#col-iowa {
     background-image: url('@images/iowa.jpg');
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: center;
-    height: 100%;
 }
 
-#col-title {
-    border: 10px solid rgb(209, 209, 209) !important;
+#col-title, .col-image {
+    /* border: 10px solid rgb(209, 209, 209) !important; */
+    /* border-radius: 3%; */
     z-index: 1;
-    border-radius: 3%;
-    background-color: rgba(240, 248, 255, 0.329);
+    /* background-color: rgba(240, 248, 255, 0.329); */
     backdrop-filter: blur(10px);
 }
 
 
 .col-md {
-    border: 1px solid rgb(37, 37, 37);
-    border-radius: 2%;
-    height: 280px;
+    /* border: 1px solid rgb(37, 37, 37); */
+    /* border-radius: 2%; */
+    height: 30vh;
     display: grid;
     place-items: center;
     padding-left: 0%;
     padding-right: 0%;
     transition: 0.5s;
-    margin: 5px 5px 5px 5px;
+    /* margin: 5px 5px 5px 5px; */
 }
-.col-choice:hover .col-image {
-    background-size: cover;
 
-    height: 500px;
+#col-p:hover, #col-exp:hover, #col-printer:hover, #col-iowa:hover{
+    transform: scale(1.1);
+    z-index: 5;
     transition: .5s;
 }
 
@@ -180,6 +167,7 @@ export default{
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center;
+    
     
  }
  
